189234952560f32ee1cffe00ff1a21ea6c2c6649
bwbl <theophile2006@gmail.com>
2025-03-24 14:46:12 +0100

    exercices are updated

--- main.js
+++ main.js
@@ -3,3 +3,7 @@ let site = document.querySelector("#site")
 document.querySelector('#exercises-button').addEventListener('click', function() {
     site.classList.toggle('hide');
 })
+
+function copy(e) {
+    navigator.clipboard.writeText(e.innerText)
+}
